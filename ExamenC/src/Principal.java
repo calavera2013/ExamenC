a8d6ae3e29d49047294ad2c44848311460d89acf
MAC <porrito@192.168.83.3>
2014-04-06 21:53:28 +0200

    Subiendo proyecto...

---
+++ ExamenC/src/Principal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		Scanner leer = new Scanner(System.in);
+		
+		// leer primer numero
+		
+		// leer segundo numero
+		
+		
+		// sumar
+		System.out.println( "La suma de " + a + " y " + b + " es " +sumar(a,b) );
+		
+		// dividir
+		// ...
+
+	}
+
+}
