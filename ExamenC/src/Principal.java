f08348a4e1bde914e35de59125e58880d55d1f42
javi0492 <javiergonzalezdam@gmail.com>
2014-04-07 09:57:28 +0200

    EjercicioC JavierGonzalez

--- ExamenC/src/Principal.java
+++ ExamenC/src/Principal.java
@@ -9,16 +9,28 @@ public class Principal {
 		Scanner leer = new Scanner(System.in);
 		
 		// leer primer numero
-		
+		a = Integer.parseInt(leer.nextLine());
 		// leer segundo numero
-		
+		b = Integer.parseInt(leer.nextLine());
 		
 		// sumar
 		System.out.println( "La suma de " + a + " y " + b + " es " +sumar(a,b) );
 		
 		// dividir
 		// ...
-
+		System.out.println( "La division de " + a + " y " + b + " es " +dividir(a,b) );
 	}
-
+	public static int sumar(int a,int b){
+		return a + b;
+	}
+	
+	public static int dividir(int a,int b){
+		if (b==0){
+			System.out.println("No se puede dividir entre 0");
+			return 0;
+		}else{
+			return a/b;
+		}
+		
+	}
 }
