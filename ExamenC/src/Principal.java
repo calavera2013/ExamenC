10373e3d16dd2671fc386c87ab78ca55f23ce102
calavera <calavera@MAC>
2014-04-07 09:49:25 +0200

    El commit de la version 1.1

--- ExamenC/src/Principal.java
+++ ExamenC/src/Principal.java
@@ -9,16 +9,29 @@ public class Principal {
 		Scanner leer = new Scanner(System.in);
 		
 		// leer primer numero
-		
+		try{
+		int a = Integer.parseInt(leer.nextLine());
+		}catch(Exception e){
+			System.out.println("Error debe introducir un numero entero para poder realizar las operaciones");
+		}
 		// leer segundo numero
-		
-		
+		try{
+		int b = Integer.parseInt(leer.nextLine());
+		}catch(Exception e){
+			System.out.println("Error debe introducir un numero entero para poder realizar las operaciones");
+		}
 		// sumar
 		System.out.println( "La suma de " + a + " y " + b + " es " +sumar(a,b) );
 		
+		public static int sumar(int a, int b){
+			return a+b;
+		}
 		// dividir
+		System.out.println("La division de " + a + " y " + b + " es " +dividir(a,b));
 		// ...
-
+		public static int dividir(int a, int b){
+			return a/b;
+		}
 	}
 
 }
